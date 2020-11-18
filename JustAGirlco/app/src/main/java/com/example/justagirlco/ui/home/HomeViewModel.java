98c281a477fd4780472d09424a0f119f86e914da
Collin Wrobel <collin.wrobel@marquette.edu>
2020-11-17 23:50:47 -0600

    Add temporary home screen

--- JustAGirlco/app/src/main/java/com/example/justagirlco/ui/home/HomeViewModel.java
+++ JustAGirlco/app/src/main/java/com/example/justagirlco/ui/home/HomeViewModel.java
@@ -1,5 +1,6 @@
 package com.example.justagirlco.ui.home;
 
+import android.widget.ImageView;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
@@ -10,7 +11,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Welcome to Girl Talk Radio!");
     }
 
     public LiveData<String> getText() {
