573d1706ff6874f704fbaef34423478f0d8bbc99
laureja <jack.laurenson@marquette.edu>
2020-11-18 12:42:16 -0600

    Got rid of android studio default header and changed names of drawers

--- JustAGirlco/app/src/main/java/com/example/justagirlco/MainActivity.java
+++ JustAGirlco/app/src/main/java/com/example/justagirlco/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.justagirlco;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
 import androidx.navigation.NavController;
@@ -31,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_mediaPage)
+                R.id.nav_home, R.id.nav_podcasts, R.id.nav_Girl_Talk, R.id.nav_mediaPage)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
