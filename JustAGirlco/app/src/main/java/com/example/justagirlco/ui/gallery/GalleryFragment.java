a13b5402910ba73167a2c03d7923a5ccff6a8fc4
Collin Wrobel <collin.wrobel@marquette.edu>
2021-02-28 15:28:39 -0600

    EXAMPLE Button navigation

--- JustAGirlco/app/src/main/java/com/example/justagirlco/ui/gallery/GalleryFragment.java
+++ JustAGirlco/app/src/main/java/com/example/justagirlco/ui/gallery/GalleryFragment.java
@@ -18,10 +18,15 @@ public class GalleryFragment extends Fragment {
 
     private GalleryViewModel galleryViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,@Nullable Bundle savedInstanceState) {
+        galleryViewModel = new ViewModelProvider(this).get(GalleryViewModel.class);
         View root = inflater.inflate(R.layout.fragment_gallery, container, false);
         final TextView textView = root.findViewById(R.id.text_gallery);
         galleryViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
--- JustAGirlco/app/src/main/java/com/example/justagirlco/ui/slideshow/SlideshowFragment.java
+++ JustAGirlco/app/src/main/java/com/example/justagirlco/ui/slideshow/SlideshowFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,17 +12,24 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
 import com.example.justagirlco.R;
 
-public class SlideshowFragment extends Fragment {
+public class SlideshowFragment extends Fragment implements View.OnClickListener {
 
     private SlideshowViewModel slideshowViewModel;
+    Button gotoPodcast;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        slideshowViewModel =
-                new ViewModelProvider(this).get(SlideshowViewModel.class);
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,@Nullable Bundle savedInstanceState) {
+        slideshowViewModel = new ViewModelProvider(this).get(SlideshowViewModel.class);
         View root = inflater.inflate(R.layout.fragment_slideshow, container, false);
         final TextView textView = root.findViewById(R.id.text_slideshow);
         slideshowViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -30,6 +38,15 @@ public class SlideshowFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        gotoPodcast = root.findViewById(R.id.fragment_girlTalk_gotoPodcasts);
+        gotoPodcast.setOnClickListener(this);
+
         return root;
+    }
+
+    @Override
+    public void onClick(View view) {
+        Navigation.findNavController(view).navigate(R.id.action_nav_girl_talk_to_nav_podcasts);
     }
 }
