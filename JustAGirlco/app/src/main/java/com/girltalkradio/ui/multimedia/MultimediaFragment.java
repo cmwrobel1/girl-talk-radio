c31662d9d8d09e754081f4aa946cdc47272248eb
Collin Wrobel <collin.wrobel@marquette.edu>
2021-03-16 20:49:54 -0500

    Pull image from firebase

--- JustAGirlco/app/src/main/java/com/girltalkradio/ui/multimedia/MultimediaFragment.java
+++ JustAGirlco/app/src/main/java/com/girltalkradio/ui/multimedia/MultimediaFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,23 +15,40 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.girltalkradio.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class MultimediaFragment extends Fragment {
 
     private MultimediaViewModel multimediaViewModel;
+    ImageView multImage;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        multimediaViewModel =
-                new ViewModelProvider(this).get(MultimediaViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        multimediaViewModel = new ViewModelProvider(this).get(MultimediaViewModel.class);
         View root = inflater.inflate(R.layout.fragment_multimedia, container, false);
-        final TextView textView = root.findViewById(R.id.text_multimedia);
-        multimediaViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        multImage = root.findViewById(R.id.multimedia_image);
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference databaseReference = firebaseDatabase.getReference("multimedia");
+        DatabaseReference getImage = databaseReference.child("profilePic").child("imageUrl");
+
+        getImage.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String link = snapshot.getValue(String.class);
+                Picasso.get().load(link).into(multImage);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getActivity(), "Error Loading Image", Toast.LENGTH_SHORT).show();
             }
         });
+
         return root;
     }
 }
