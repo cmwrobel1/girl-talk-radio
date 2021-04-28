bf40d0c84ced1442ac94b2eeb1c472a41e4fb9b3
Katie Tooher <44885677+kt3a@users.noreply.github.com>
2021-04-28 00:22:10 -0500

    recycler firebase adapter for top 10 page

---
+++ JustAGirlco/app/src/main/java/com/girltalkradio/Podcast.java
@@ -0,0 +1,36 @@
+package com.girltalkradio;
+
+public class Podcast {
+
+    private String rss;
+    private String title;
+    private Long order;
+    private String image;
+
+    public Podcast(){
+
+    }
+
+    public Podcast(String url, String title, Long order,String image){
+        this.rss = url;
+        this.title = title;
+        this.order = order;
+        this.image = image;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+    public String getRss(){
+        return rss;
+    }
+    public Long getOrder(){
+        return order + 1;
+    }
+    public String getImage(){return image;}
+    public void setImage(String image){this.image = image;}
+    public void setTitle(String title) {this.title = title;}
+    public void setRss(String rss){this.rss = rss;}
+    public void setOrder(Long order){this.order = order;}
+
+}
--- JustAGirlco/app/src/main/java/com/girltalkradio/ui/top10/Top10Fragment.java
+++ JustAGirlco/app/src/main/java/com/girltalkradio/ui/top10/Top10Fragment.java
@@ -11,25 +11,81 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.girltalkradio.Podcast;
 import com.girltalkradio.R;
+import com.girltalkradio.Top10Adapter;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class Top10Fragment extends Fragment {
+import java.util.ArrayList;
+
+public class Top10Fragment extends Fragment implements Top10Adapter.onPodcastListener {
 
     private Top10ViewModel top10ViewModel;
+    Top10Adapter adapter;
+    RecyclerView recview;
+    FirebaseRecyclerOptions<Podcast> options;
+    private Top10Adapter.onPodcastListener podcastListener;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         top10ViewModel =
                 new ViewModelProvider(this).get(Top10ViewModel.class);
         View root = inflater.inflate(R.layout.fragment_top_10, container, false);
-        final TextView textView = root.findViewById(R.id.text_top_10);
-        top10ViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+
+        recview = (RecyclerView)root.findViewById(R.id.recview);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReferenceFromUrl("https://justagirlco-b4de1-default-rtdb.firebaseio.com/1StAH6C83cDbx5l8WidtkteVUpwXHi2DBcpJtd4WGpCY/top10");
+
+                myRef.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                ArrayList<Podcast> pods = (ArrayList<Podcast>) snapshot.getValue();
+
+                System.out.println(pods);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
+        podcastListener = this;
+        options = new FirebaseRecyclerOptions.Builder<Podcast>().setQuery(myRef, Podcast.class).build();
+
+        adapter = new Top10Adapter(options,podcastListener);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(),1,GridLayoutManager.VERTICAL,false);
+        recview.setLayoutManager(gridLayoutManager);
+        recview.setAdapter(adapter);
+
         return root;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
+    @Override
+    public void onPodcastListner(int position) {
+
+    }
+
 }
